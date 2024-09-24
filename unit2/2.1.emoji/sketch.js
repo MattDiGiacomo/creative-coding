936d17c88af0e8e5744ded79210482db4b14c624
MattDiGiacomo <mdigiaco@mail.umw.edu>
2024-09-24 10:57:03 -0400

    Update sketch.js

--- unit2/2.1.emoji/sketch.js
+++ unit2/2.1.emoji/sketch.js
@@ -6,9 +6,42 @@ function setup() {
   noLoop();
 }
 
-function draw() {
-  background(Math.floor(Math.random() * 256));
+function draw(circle) {
+  background(220);
 
   rectMode(CENTER);
  
+}
+
+function setup() {
+  createCanvas(400, 400);
+  background(255);
+  drawAngerEmoji();
+}
+
+function drawAngerEmoji() {
+  // Face
+  fill(255, 0, 0);
+  ellipse(200, 200, 200, 200);
+
+  // Eyes
+  fill(0);
+  ellipse(160, 170, 30, 30);
+  ellipse(240, 170, 30, 30);
+
+  // Eyebrows
+  stroke(0);
+  strokeWeight(5);
+  line(140, 150, 180, 160);
+  line(220, 160, 260, 150);
+
+  // Mouth
+  noFill();
+  stroke(0);
+  strokeWeight(5);
+  arc(200, 240, 100, 50, 0, PI);
+}
+
+function draw() {
+  // No continuous drawing needed
 }
