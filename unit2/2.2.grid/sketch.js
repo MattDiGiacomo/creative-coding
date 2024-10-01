6d62138613d54664e3980fc8add6ee5a99d0db0c
MattDiGiacomo <mdigiaco@mail.umw.edu>
2024-10-01 11:09:25 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -17,8 +17,10 @@ circle(x * 50,80,50);
 }
 for (let x = 1; x < 9; x += 1){
 for (let y = 1; y < 9; y+=1){
-
-circle(x * 50, y * 50,50);
+fill(random(255), random(255), random(255));
+strokeWeight(random(1,15))
+stroke(random(0,200),25,235)
+circle(x * 60, y * 60,60);
 
 }
 
