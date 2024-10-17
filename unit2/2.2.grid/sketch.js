65ebc23217e4eca2ec6f30c11dd16aba37b8061d
MattDiGiacomo <mdigiaco@mail.umw.edu>
2024-10-17 14:41:47 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -10,15 +10,15 @@ function draw() {
 
 for (let x = 0; x < 0; x += 1){
 
-circle(x * 50,80,50);
+circle(x * 50,80,50);// draw a circle
 
 }
-for (let x = 1; x < 9; x += 1){
-for (let y = 1; y < 9; y+=1){
-fill(random(255), random(255), random(255));
-strokeWeight(random(1,15))
-stroke(random(0,200),25,235)
-circle(x * 60, y * 60,60);
+for (let x = 1; x < 9; x += 1){ // looping 
+for (let y = 1; y < 9; y+=1){ // looping
+fill(random(255), random(255), random(255)); // random colors on circles
+strokeWeight(random(1,15))// random line thickness
+stroke(random(0,200),25,235)// color of the border
+circle(x * 60, y * 60,60);//
   
 }
 
