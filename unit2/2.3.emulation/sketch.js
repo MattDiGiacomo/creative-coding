43df1482a0adc6f411320cfd8b706267db3ad1bc
MattDiGiacomo <mdigiaco@mail.umw.edu>
2024-10-20 14:17:45 -0400

    Update sketch.js

--- unit2/2.3.emulation/sketch.js
+++ unit2/2.3.emulation/sketch.js
@@ -9,5 +9,16 @@ function setup() {
 function draw() {
 
   background(255);
+stroke(0);
+strokeweight(2);
+noFill();
 
+function setup() {
+createCanvas(400,400);
+}
+function draw() {
+  stroke(0);
+  strokeweight(2);
+  for (let x = 0; x<= width; x += spacing)
+  }
 }
