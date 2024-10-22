d9fbbd5ceb86cb00a1c9287b1ed6fedd1672d346
MattDiGiacomo <mdigiaco@mail.umw.edu>
2024-10-22 11:32:20 -0400

    Update sketch.js

--- unit3/3.1.bounce/sketch.js
+++ unit3/3.1.bounce/sketch.js
@@ -5,11 +5,12 @@ let yspeed = 5;
 
 function setup() {
   createCanvas(400, 300);
+    fill("blue")
 }
 
 function draw() {
   background(220);
-  fill("blue")
+
   noStroke();
 
 translate(width / 2, height / 2);
@@ -18,10 +19,12 @@ circle(x,y,20);
 
 if (x > 200 | x < -200){
   xspeed = -xspeed;
+  fill(random(255),100,100)
 }
 
 if (y > 150 | y < -150){
   yspeed = -yspeed;
+    fill(random(255),100,100)
 }
 y += yspeed
 x += xspeed;
