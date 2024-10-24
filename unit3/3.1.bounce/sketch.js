a57b7efa9a9df8b334f13111eecde0f7843c1ab9
MattDiGiacomo <mdigiaco@mail.umw.edu>
2024-10-24 11:49:25 -0400

    Update sketch.js

--- unit3/3.1.bounce/sketch.js
+++ unit3/3.1.bounce/sketch.js
@@ -1,8 +1,11 @@
-let x = 0;
-let y = 0;
-let xspeed = 5;
-let yspeed = 5;
-
+let x1 = 0;
+let x2 = 0;
+let y1 = 0;
+let y2 = 0;
+let x1speed = 5;
+let x2speed = 5;
+let y1speed = 5;
+let y2speed = 5;
 function setup() {
   createCanvas(400, 300);
     fill("blue")
@@ -15,18 +18,34 @@ function draw() {
 
 translate(width / 2, height / 2);
 
-circle(x,y,20);
+circle(x1,y1,20);
 
-if (x > 200 | x < -200){
-  xspeed = -xspeed;
+if (x1 > 200 | x1 < -200){
+  x1speed = -x1speed;
   fill(random(255),100,100)
 }
 
-if (y > 150 | y < -150){
-  yspeed = -yspeed;
+if (y1 > 150 | y1 < -150){
+  y1speed = -y1speed;
     fill(random(255),100,100)
 }
-y += yspeed
-x += xspeed;
+y1 += y1speed
+x1 += x1speed;
+
+translate(width / 2, height / 2);
+
+circle(x2,y2,20);
+
+if (x2 > 300 | x2 < -350){
+  x2speed = -x2speed;
+  fill(random(255),100,100)
+}
+
+if (y2 > 300 | y2 < -300){
+  y2speed = -y2speed;
+    fill(random(255),100,100)
+}
+y2 += y2speed;
+x2 += x2speed;
 
 }
