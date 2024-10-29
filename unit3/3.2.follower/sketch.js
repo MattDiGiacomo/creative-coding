adc240a684eeb486fa99d5ec655fd3e276301127
MattDiGiacomo <mdigiaco@mail.umw.edu>
2024-10-29 09:15:20 -0400

    Update sketch.js

--- unit3/3.2.follower/sketch.js
+++ unit3/3.2.follower/sketch.js
@@ -1,7 +1,18 @@
+let speedfactor = 3;
+let xspeed = speedfactor;
+let yspeed = speedfactor;
+let x = 0; 
+let y = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
+fill(100,150,255);
+noStroke();
+ellipse(mouseX, mouseY,50, 50);
+
+
 }
