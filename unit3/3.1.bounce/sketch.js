c2235852d94b6bb043e1b5b6bfe3a85092f51bc4
MattDiGiacomo <mdigiaco@mail.umw.edu>
2024-11-25 10:20:46 -0500

    Update sketch.js

--- unit3/3.1.bounce/sketch.js
+++ unit3/3.1.bounce/sketch.js
@@ -2,14 +2,15 @@ let x = 0;
 let y = 0;
 let xspeed = 5;
 let yspeed = 5;
+// x and y are for circle cordinates
+//xspeed and yspeed move the circle
 
-
-function setup() {
+function setup() { // creates canvas and makes the background blue
   createCanvas(400, 300);
     fill("blue")
 }
 
-function draw() {
+function draw() { //background color, no shape outlines, translate moves origin
   background(220);
 
   noStroke();
@@ -18,7 +19,7 @@ translate(width / 2, height / 2);
 
 circle(x,y,20);
 
-if (x > 200 | x < -200){
+if (x > 200 | x < -200){ // gives the circle boundaries, causing it to change direction 
   xspeed = -xspeed;
   fill(random(255),100,100);
 }
