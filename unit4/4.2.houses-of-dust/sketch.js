c5856b2fcac8ca63fe7aede98ef94e1961b4e6b4
MattDiGiacomo <mdigiaco@mail.umw.edu>
2024-11-25 18:37:05 -0500

    Update sketch.js

--- unit4/4.2.houses-of-dust/sketch.js
+++ unit4/4.2.houses-of-dust/sketch.js
@@ -1,12 +1,12 @@
 let poem = '';
 
 let grammar = tracery.createGrammar({
-"origin": ["A HOUSE OF #material#"],
-"material":["SAND","DUST","LEAVES","PAPER","TIN","ROOTS","BRICK","BROKEN DISHES", "WOOD","STRAW","WEEDS"]
-
-
+"origin": ["A HOUSE OF #material# #situation# #lighting# #inhabitants#"],
+"material":["SAND","DUST","LEAVES","PAPER","TIN","ROOTS","BRICK","BROKEN DISHES", "WOOD","STRAW","WEEDS"],
+"situation":["ON A MOUNTAIN", "IN A FOREST", "IN A CITY", "BY THE SEA", "IN A DESERT"],
+"lighting":["UNDER THE SUN", "UNDER THE MOON", "IN THE RAIN", "IN THE SNOW", "IN THE FOG"],
+"inhabitants":[ "INHABITED BY ANIMALS", "INHABITED BY PEOPLE", "INHABITED BY GHOSTS", "INHABITED BY ZOMBIES", "INHABITED BY PILLOWS"]
 });
-
 
 
 function setup() {
